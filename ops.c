250b98501ea51428bd990c2434ca1518a027cfec
conkobar <connorkleine@gmail.com>
2022-08-12 10:26:32 -0500

    betty checks

--- ops.c
+++ ops.c
@@ -53,8 +53,8 @@ void push(stack_t **stack, unsigned int line_number)
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	(void)line_number;
 	stack_t *tmp;
+	(void)line_number;
 
 	if (*stack)
 	{
