f5282e91e13b0eaae3095d12f837aa5ac4585958
Brandon <brandon_scott585@yahoo.com>
2022-08-11 19:43:01 -0700

    updated swap.c with betty also

--- swap.c
+++ swap.c
@@ -12,11 +12,11 @@ void swap(stack_t **stack, unsigned int line_num)
 	stack_t *tmp = NULL;
 	int temp = 0;
 
-	if (!stack || *stack || !((*stack)->next))
+	if (!stack || !*stack || !((*stack)->next))
 	{
-		dprintf(STDERR_FILENO, "L%d: can't swap, stack too short\n", line_num);
-				exit(EXIT_FAILURE);
-				return;
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+		return;
 	}
 
 	tmp = *stack;
